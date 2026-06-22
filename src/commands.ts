671c2e03afe455463c4860c467c143582516468a
YuXuaann <1922880669@qq.com>
2026-06-22 06:09:24 +0000

    feat(publish): update .gitignore, launch configuration, package repository info, and command execution

--- src/commands.ts
+++ src/commands.ts
@@ -16,7 +16,7 @@ export async function openWorkspace() {
         openLabel: 'Open Workspace',
     });
     if (selected && selected.length > 0) {
-        await vscode.commands.executeCommand(consts.commands.openWorkspace, selected[0]);
+        await vscode.commands.executeCommand('vscode.openFolder', selected[0]);
     }
 }
 
