cf6894a7bcc8c98dcba5ff158670f28e9f2fd912
HualiNox <hualinox@gmail.com>
2026-07-24 05:49:38 +0000

    feat(template): support custom template files
    Signed-off-by: HualiNox <hualinox@gmail.com>

--- src/configs.ts
+++ src/configs.ts
@@ -1,6 +1,7 @@
 import * as consts from './consts';
 import * as meta from './contest/meta';
 import { Result, Ok } from "./utils";
+import * as fs from 'fs';
 
 export enum LanguageType {
     C = 'c/cpp',
@@ -80,24 +81,66 @@ export class CompilerConfig {
 }
 
 export class RunnerConfig {
-    public readonly template: string;
 
     constructor(
         public readonly timeLimitSecond: number,
         public readonly memoryLimitMB: number,
+        public readonly template: string,
+    ) {}
+
+    static async create(
+        timeLimitSecond: number,
+        memoryLimitMB: number,
         templateMode: string,
         customTemplate: string,
+        customTemplateFile: string,
     ) {
-        switch (templateMode) {
+        let template: string;
+        switch (templateMode ) {
             case TemplateMode.custom:
-                this.template = customTemplate;
+                if (customTemplateFile) {
+                    template = await this.loadCustomTemplate(customTemplateFile);
+                    break;
+                }
+
+                template = customTemplate;
                 break;
             case TemplateMode.auto:
             default:
-                this.template = consts.AUTO_CODE_TEMPLATE;
+                template = consts.AUTO_CODE_TEMPLATE;
                 break;
+        }
+
+        return new RunnerConfig(
+            timeLimitSecond,
+            memoryLimitMB,
+            template
+        );
+    }
+
+    private static async loadCustomTemplate(
+        templatePath: string
+    ): Promise<string> {
+
+        if (!templatePath) {
+            throw new Error(
+                "Custom template path is empty"
+            );
+        }
+
+        try {
+            return await fs.promises.readFile(
+                templatePath,
+                {
+                    encoding: "utf8"
+                }
+            );
+        } catch (error) {
+            throw new Error(
+                `Failed to read template: ${error}`
+            );
         }
     }
 }
 
-export const elycodeConfig = new ElycodeConfig(); 
+export const elycodeConfig = new ElycodeConfig();
